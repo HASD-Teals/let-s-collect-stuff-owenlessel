ff3b76937a64174810de22bd7cbeaf3010aebed7
owenlessel <owenlessel@gmail.com>
2022-02-15 10:04:06 -0600

    test

--- lets-collect-stuff/src/CollectionClient.java
+++ lets-collect-stuff/src/CollectionClient.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CollectionClient {
     public static void main(String[] args) throws Exception {
         // Add your collection ArrayList here..
         // e.g. List<Collectable> collection = new ArrayList<Collectable>();
-
-        // Add collection methods here to manage and manipulate the collection
+        List<Coin> Coins = new ArrayList<Coin>();
+        // Add collection methods here to manage and manipulate the collection 
+        
     }
 }
